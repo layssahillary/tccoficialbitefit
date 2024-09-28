24deca3af81e9b21c2bd06cc12234b511777c2b7
Layssa <layssahillary@hotmail.com>
2024-09-28 14:55:17 -0300

    fix: ajustando caminho do arquivo

--- frontend/my-react-app/src/pages/PatientsPage/PatientsPage.jsx
+++ frontend/my-react-app/src/pages/PatientsPage/PatientsPage.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import Header from '../../components/Header/index';
 import Footer from '../../components/Footer/index';
-import PatientsPageView from '../../components/Views/PatientsPageView/PateintsPageView';
+import PatientsPageView from '../../components/Views/PatientsPageView/PatientsPageView';
 
 const PatientsPage = () => {
   return (
